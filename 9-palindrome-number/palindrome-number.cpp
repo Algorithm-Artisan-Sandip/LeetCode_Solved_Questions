2e9f2232f8cc22f6422516ca6cc7718a9c101dfb
Sandip Pramanik <163902046+Algorithm-Artisan-Sandip@users.noreply.github.com>
2024-04-13 10:52:47 +0530

    Time: 12 ms (34.05%) | Memory: 9.1 MB (15.60%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0) return false;
+        string str = to_string(x);
+        int i=0, j=str.size()-1;
+        while(i<j){
+            if(str[i]==str[j]){
+                i++; j--;
+            }
+            else return false;
+        }
+        return true;
+    }
+};
