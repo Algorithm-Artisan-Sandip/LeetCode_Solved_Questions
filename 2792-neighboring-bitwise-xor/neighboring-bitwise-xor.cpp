dbcf815133e4f2a5cef7d3061b07e77a6d76dbd4
Sandip Pramanik <163902046+Algorithm-Artisan-Sandip@users.noreply.github.com>
2024-04-14 11:48:52 +0530

    Time: 193 ms (80.00%) | Memory: 262.7 MB (75.71%) - LeetSync

--- 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
@@ -1,9 +1,15 @@
 class Solution {
 public:
     bool doesValidArrayExist(vector<int>& derived) {
+        
         int check = 0;
-        for(int i=0; i<derived.size(); i++) check^=derived[i];
+
+        int size = derived.size();
+
+        for(int i=0; i<size; i++) check^=derived[i];
+
         if(check==0) return true;
+
         else return false;
     }
 };
