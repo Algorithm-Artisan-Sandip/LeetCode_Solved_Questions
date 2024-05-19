f937d26c665c25f9d1f3760b4903700fcebdaf18
Sandip Pramanik <163902046+Algorithm-Artisan-Sandip@users.noreply.github.com>
2024-05-19 10:20:36 +0530

    Time: 18 ms (56.46%) | Memory: 22 MB (67.86%) - LeetSync

---
+++ 2350-find-closest-number-to-zero/find-closest-number-to-zero.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findClosestNumber(vector<int>& nums) {
+        int res = nums[0];
+        for(int i = 1; i < nums.size();i++){
+            int diff = abs(res - 0);
+            int curr = abs(nums[i] - 0);
+
+            if(curr < diff){
+                res = nums[i];
+            }
+            if(curr == diff){
+                res = max(res, nums[i]);
+            }
+        }
+        return res;
+    }
+};
