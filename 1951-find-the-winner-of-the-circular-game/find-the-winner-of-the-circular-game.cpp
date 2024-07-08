57b09a6f23c8c5077ac9912967813a42ccb78b39
Sandip Pramanik <163902046+Algorithm-Artisan-Sandip@users.noreply.github.com>
2024-07-08 09:40:47 +0530

    Time: 3 ms (65.36%) | Memory: 8.4 MB (38.03%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,21 +1,48 @@
+// JOSEPHUS Problem : 
+
+// Approach 1 :
+
+// class Solution {
+// public:
+//     void findAndDel(queue<int>& q, int k) {
+//         for(int i=0; i <k-1; i++) {
+//             int temp = q.front();
+//             q.pop();
+//             q.push(temp);
+//         }
+//         q.pop();
+//     }
+//     int findTheWinner(int n, int k) {
+//         queue<int> q;
+//         for(int i=0; i<n; i++) {
+//             q.push(i+1);
+//         }
+//         while(q.size()>1) {
+//             findAndDel(q,k);
+//         }
+//         return q.front();
+//     }
+// };
+
+// Approach 2 : 
+
 class Solution {
 public:
-    void findAndDel(queue<int>& q, int k) {
-        for(int i=0; i <k-1; i++) {
-            int temp = q.front();
-            q.pop();
-            q.push(temp);
+    int findAndDel(int n, int k) {
+        // base case : at last one element remains so return the index of the element
+        if(n==1) {
+            return 0;
         }
-        q.pop();
+        
+        else {
+            return (findAndDel(n-1,k)+k) % n;  // most important => after each operation one element will be deleted(so n-1) and the couning will start after the kth person(so +k) and to maintain the circular nature %n.
+        }
     }
     int findTheWinner(int n, int k) {
         queue<int> q;
         for(int i=0; i<n; i++) {
             q.push(i+1);
         }
-        while(q.size()>1) {
-            findAndDel(q,k);
-        }
-        return q.front();
+        return findAndDel(n,k) + 1;
     }
 };
