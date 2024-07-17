73015fc60540558d6ad707f443a3dbe6d2d297fd
Sandip Pramanik <163902046+Algorithm-Artisan-Sandip@users.noreply.github.com>
2024-07-17 20:55:08 +0530

    Time: 4 ms (77.02%) | Memory: 18 MB (5.43%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -9,15 +9,54 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+
+// Approach 1 : using recursion.
+// class Solution {
+// public:
+//     int maxDepth(TreeNode* root) {
+//         if(root == NULL) return 0;
+
+//         int leftSubtreeDepth = maxDepth(root->left);
+//         int rightSubtreeDepth = maxDepth(root->right);
+//         int maxDepth = max(leftSubtreeDepth, rightSubtreeDepth) + 1;
+
+//         return maxDepth;
+//     }
+// };
+
+
+// Approach 2 : using level order traversal.
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
         if(root == NULL) return 0;
+        
+        int count = 1;
+        queue<TreeNode*> q;
+        q.push(root);
+        q.push(NULL);
 
-        int leftSubtreeDepth = maxDepth(root->left);
-        int rightSubtreeDepth = maxDepth(root->right);
-        int maxDepth = max(leftSubtreeDepth, rightSubtreeDepth) + 1;
+        while(!q.empty()) {
+            TreeNode* currNode = q.front();
+            q.pop();
 
-        return maxDepth;
+            if(currNode == NULL) {
+                if(!q.empty()) {
+                    q.push(NULL);
+                    count++;
+                }
+            }
+
+            else {
+                if(currNode->left != NULL) {
+                    q.push(currNode->left);
+                }
+                if(currNode->right !=  NULL) {
+                    q.push(currNode->right);
+                }
+            }
+        }
+        return count;
     }
 };
