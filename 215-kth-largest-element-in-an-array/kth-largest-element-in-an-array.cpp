c8c74c30bf644afb7a059d1713e521b105f93cd0
Sandip Pramanik <163902046+Algorithm-Artisan-Sandip@users.noreply.github.com>
2024-08-10 11:12:01 +0530

    Time: 84 ms (79.45%) | Memory: 61.6 MB (30.03%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -8,19 +8,42 @@
 //     }
 // };
 
-// Approach 2  : O(n), using priority_queue : 
+// Approach 2  : O(nlogn), using priority_queue : 
+// class Solution {
+// public:
+//     int findKthLargest(vector<int>& nums, int k) {
+//         priority_queue<int> pq;
+
+//         for(auto &temp : nums) {
+//             pq.push(temp);
+//         }
+
+//         while(k-1) {
+//             pq.pop();
+//             k--;
+//         }
+
+//         return pq.top();
+//     }
+// };
+
+// Approach 3 : efficient space complexity : 
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int> pq;
+        priority_queue<int, vector<int>, greater<int>> pq;
 
-        for(auto &temp : nums) {
-            pq.push(temp);
+        // pushing first k elements in min_heap :
+        for(int i=0; i<k; i++) {
+            pq.push(nums[i]);
         }
 
-        while(k-1) {
-            pq.pop();
-            k--;
+        // now processing other elements : 
+        for(int i=k; i<nums.size(); i++) {
+            if(nums[i] > pq.top()) {
+                pq.pop();
+                pq.push(nums[i]);
+            }
         }
 
         return pq.top();
