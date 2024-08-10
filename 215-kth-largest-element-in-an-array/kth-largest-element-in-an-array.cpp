d945b34bcf0827efea17b7934f1738df3a7be9c8
Sandip Pramanik <163902046+Algorithm-Artisan-Sandip@users.noreply.github.com>
2024-08-10 11:02:06 +0530

    Time: 118 ms (9.70%) | Memory: 64.7 MB (7.27%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,8 +1,28 @@
+// Approach 1 : O(nlogn), using sort function :
+// class Solution {
+// public:
+//     int findKthLargest(vector<int>& nums, int k) {
+//         sort(nums.begin(),nums.end());
+//         int idx = nums.size()-k;
+//         return nums[idx];
+//     }
+// };
+
+// Approach 2  : O(n), using priority_queue : 
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        sort(nums.begin(),nums.end());
-        int idx = nums.size()-k;
-        return nums[idx];
+        priority_queue<int> pq;
+
+        for(auto &temp : nums) {
+            pq.push(temp);
+        }
+
+        while(k-1) {
+            pq.pop();
+            k--;
+        }
+
+        return pq.top();
     }
 };
