789978f016b8d30d14bd8dc090b50c194e8d5818
Sandip Pramanik <163902046+Algorithm-Artisan-Sandip@users.noreply.github.com>
2024-08-12 07:05:37 +0530

    Time: 17 ms (54.42%) | Memory: 18.4 MB (37.70%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -3,8 +3,8 @@
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode() : val(0), next(NULL) {}
+ *     ListNode(int x) : val(x), next(NULL) {}
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
@@ -24,24 +24,24 @@ public:
         // Process the first element of every Linked List
         for(int i = 0; i < lists.size(); i++) {
             ListNode* listHead = lists[i];
-            if(listHead != nullptr) {
+            if(listHead != NULL) {
                 pq.push(listHead);
             }
         }
 
         // If the priority queue is empty, return NULL
-        if (pq.empty()) return nullptr;
+        if (pq.empty()) return NULL;
 
         // New Linked List
-        ListNode* head = nullptr;
-        ListNode* tail = nullptr;
+        ListNode* head = NULL;
+        ListNode* tail = NULL;
 
         while(!pq.empty()) {
             ListNode* topNode = pq.top();
             pq.pop();
 
             // If the new linked list is empty, initialize it with the topNode
-            if(head == nullptr) {
+            if(head == NULL) {
                 head = topNode;
                 tail = topNode;
             } else {
@@ -51,7 +51,7 @@ public:
             }
 
             // If there's a next node in the current list, push it into the priority queue
-            if(topNode->next != nullptr) {
+            if(topNode->next != NULL) {
                 pq.push(topNode->next);
             }
         }
