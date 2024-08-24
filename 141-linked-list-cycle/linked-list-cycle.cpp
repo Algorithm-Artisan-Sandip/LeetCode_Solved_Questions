674e7156c22e87e4f2f6619bf22dc3976a310ae2
Sandip Pramanik <163902046+Algorithm-Artisan-Sandip@users.noreply.github.com>
2024-08-24 11:27:48 +0530

    Time: 17 ms (9.53%) | Memory: 15.2 MB (5.39%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -6,18 +6,40 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+// Approach 1 : Using Map : 
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        ListNode* slow = head;
-        ListNode* fast = head;
-        while(fast != NULL) {
-            fast = fast->next;
-            slow = slow->next;
-            if(fast != NULL) fast = fast->next;
-            else return false;
-            if(slow==fast) return true;
+        unordered_map<ListNode*, bool> mapping;
+        ListNode* temp = head;
+        while(temp != NULL) {
+            // cycle present : 
+            if(mapping.find(temp) != mapping.end()) return true;
+            // cycle not present : 
+            else {
+                mapping[temp] = true;
+            }
+            temp = temp->next;
         }
         return false;
     }
 };
+
+
+// Approach 2 : Tortoise and Hare algorithm :
+// class Solution {
+// public:
+//     bool hasCycle(ListNode *head) {
+//         ListNode* slow = head;
+//         ListNode* fast = head;
+//         while(fast != NULL) {
+//             fast = fast->next;
+//             slow = slow->next;
+//             if(fast != NULL) fast = fast->next;
+//             else return false;
+//             if(slow==fast) return true;
+//         }
+//         return false;
+//     }
+// };
