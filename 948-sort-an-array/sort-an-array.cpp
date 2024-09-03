b7965b4f8daf7642a390fb96e2236905948c6502
Sandip Pramanik <163902046+Algorithm-Artisan-Sandip@users.noreply.github.com>
2024-09-04 00:58:47 +0530

    Time: 575 ms (19.60%) | Memory: 284.4 MB (10.33%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,7 +1,48 @@
 class Solution {
 public:
+    void merge(vector<int>& arr, int low, int mid, int high) {
+        int left = low;
+        int right = mid+1;
+        vector<int> temp;
+
+        while(left <= mid && right <= high) {
+            if(arr[left] <= arr[right]) {
+                temp.push_back(arr[left]);
+                left++;
+            }
+            else {
+                temp.push_back(arr[right]);
+                right++;
+            }
+        }
+
+        while(left <= mid) {
+            temp.push_back(arr[left]);
+            left++;
+        }
+
+        while(right <= high) {
+            temp.push_back(arr[right]);
+            right++;
+        }
+
+        for(int i=low; i<=high; i++) {
+            arr[i] = temp[i-low];
+        }
+
+    }
+    void msHelper(vector<int>& arr, int low, int high) {
+        if(low == high) return;
+
+        int mid = low + (high - low) / 2;
+
+        msHelper(arr, low, mid);
+        msHelper(arr, mid+1, high);
+
+        merge(arr, low, mid, high);
+    }
     vector<int> sortArray(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        msHelper(nums, 0, nums.size()-1);
         return nums;
     }
 };
