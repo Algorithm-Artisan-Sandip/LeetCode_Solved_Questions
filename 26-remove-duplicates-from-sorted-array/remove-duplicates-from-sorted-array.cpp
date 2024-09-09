0b8ba8792d7957644164eba07f40648115af9ebe
Sandip Pramanik <163902046+Algorithm-Artisan-Sandip@users.noreply.github.com>
2024-09-09 19:09:51 +0530

    Time: 10 ms (37.84%) | Memory: 21.4 MB (59.81%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,18 +1,42 @@
+// Approach 2 : Two Pointer : T.C: O(n), S.C: O(1)
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        unordered_set<int> seen;
+        int size = nums.size();
 
         int pos = 0;
-        for(auto& it : nums) {
-            if(seen.find(it) == seen.end()) {
-                seen.insert(it);
-                nums[pos++] = it;
+        for(int i=0; i<size; i++) {
+            if(nums[pos] != nums[i]) {
+                nums[++pos] = nums[i];
             }
         }
 
-        nums.resize(pos);
-
-        return pos;
+        nums.resize(pos+1);
+        return pos+1;
     }
 };
+
+
+
+
+
+// Approach 1 : Using Unordered Set : T.C: O(n), S.C: O(n)
+// class Solution {
+// public:
+//     int removeDuplicates(vector<int>& nums) {
+//         unordered_set<int> seen;
+
+//         int pos = 0;
+//         for(auto& it : nums) {
+//             if(seen.find(it) == seen.end()) {
+//                 seen.insert(it);
+//                 nums[pos++] = it;
+//             }
+//         }
+
+//         nums.resize(pos);
+
+//         return pos;
+//     }
+// };
+
