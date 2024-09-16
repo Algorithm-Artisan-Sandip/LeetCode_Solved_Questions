52cd906827de3d209a8d235723c551d280c72593
Sandip Pramanik <163902046+Algorithm-Artisan-Sandip@users.noreply.github.com>
2024-09-16 18:40:01 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (59.42%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -1,16 +1,34 @@
+// Approach 2 : Optimal --> Without using an extra array : 
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int row = matrix.size();
-        for(int i=0; i<row; i++){
-            for(int j=0; j<row; j++){
-                if(i>j){
-                    swap(matrix[i][j],matrix[j][i]);
-                }
-            }
-        }
-        for(int i=0; i<row; i++){
-             reverse(matrix[i].begin(),matrix[i].end());
-        }
+        int n = matrix.size();
+
+        // step 1 : transposing the matrix : 
+        for(int i=0; i<n; i++) 
+            for(int j=0; j<n && i != j; j++) 
+                swap(matrix[i][j], matrix[j][i]);
+
+        // step 2 : reversing each row of the matrix : 
+        for(int i=0; i<n; i++)
+            reverse(matrix[i].begin(), matrix[i].end());
     }
 };
+
+
+// // Approach 1 : Brute Force --> Using an extra array : 
+// class Solution {
+// public:
+//     void rotate(vector<vector<int>>& matrix) {
+//         int n = matrix.size();
+//         vector<vector<int>> rotated(n, vector<int>(n,0));
+	    
+// 	    for (int i = 0; i < n; i++)
+//             for (int j = 0; j < n; j++) 
+//                 rotated[j][n - i - 1] = matrix[i][j];
+
+//         for(int i = 0; i < n; i++)
+//             for(int j = 0; j < n; j++)
+//                 matrix[i][j] = rotated[i][j];
+//     }
+// };
