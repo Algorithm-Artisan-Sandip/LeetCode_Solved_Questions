fa9b73bc74b2af9e2baff4f91d08e56b72958545
Sandip Pramanik <163902046+Algorithm-Artisan-Sandip@users.noreply.github.com>
2024-10-06 10:48:07 +0530

    Time: 9 ms (68.21%) | Memory: 15.8 MB (9.32%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,14 +1,37 @@
+// Approach 2 : T.C.=O(n), S.C.=O(n) : Using map : 
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> seen;
-        for (int i = 0; i < nums.size(); ++i) {
-            int complement = target - nums[i];
-            if (seen.count(complement)) {
-                return {seen[complement], i};
-            }
-            seen[nums[i]] = i;
+        // creating the entries of each element in the map : 
+        unordered_map<int,int> mpp;
+        for(int i=0; i<nums.size(); i++) {
+            mpp[nums[i]] = i;
         }
-        return {};
+
+        // checking if the target pair exists or not : 
+        for(int i=0; i<nums.size(); i++) {
+            int more = target - nums[i];
+            if(mpp.find(more) != mpp.end() && mpp[more] != i) {
+                return {i, mpp[more]};
+            }
+        }
+
+        return {-1, -1};
     }
 };
+
+
+
+// Approach 1 : T.C.=O(n^2), S.C.=O(1) : 
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         int size = nums.size();
+//         for(int i=0; i<size; i++)
+//             for(int j=0; j<size; j++)
+//                 if(j!=i && nums[i]+nums[j] == target)
+//                     return {i, j};
+
+//         return {-1, -1};
+//     }
+// };
