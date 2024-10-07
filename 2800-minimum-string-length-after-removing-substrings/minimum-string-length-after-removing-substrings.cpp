be5d795155ad93c34f055e0f591c3685147335d8
Sandip Pramanik <163902046+Algorithm-Artisan-Sandip@users.noreply.github.com>
2024-10-07 17:45:42 +0530

    Time: 12 ms (15.77%) | Memory: 12.1 MB (7.22%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
@@ -2,22 +2,14 @@ class Solution {
 public:
     int minLength(string s) {
         stack<int> st;
-        for(int i=0; i<s.size(); i++) {
-            st.push(s[i]);
-            if(st.size()>1) {
-                if(st.top() == 'B') {
-                    char ch = st.top();
+        for(auto ch : s) {
+            if(!st.empty()) {
+                if((ch == 'B' && st.top() == 'A') || (ch == 'D' && st.top() == 'C')) {
                     st.pop();
-                    if(st.top() == 'A') st.pop();
-                    else st.push(ch);
-                }
-                else if(st.top() == 'D') {
-                    char ch = st.top();
-                    st.pop();
-                    if(st.top() == 'C') st.pop();
-                    else st.push(ch);
+                    continue;
                 }
             }
+            st.push(ch);
         }
 
         return st.size();
