f4e1916f369a24072759baf1cb085a194368777d
Sandip Pramanik <163902046+Algorithm-Artisan-Sandip@users.noreply.github.com>
2025-02-04 23:38:36 +0530

    Time: 3 ms (4.86%) | Memory: 11.6 MB (34.58%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -1,26 +1,14 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        int cdigitsy = 0;
         for(int i=digits.size()-1; i>=0; i--) {
-            if(digits[i] == 9) {
-                digits[i] = 0;
-                cdigitsy = 1;
-                continue;
+            if(digits[i] < 9) {
+                digits[i]++;
+                return digits;
             }
-            else {
-                digits[i] += 1;
-                break;
-            }
+            digits[i] = 0;
         }
-        if(digits[0] == 0) {
-            vector<int> ans(digits.size()+1);
-            ans[0] = 1;
-            for(int i=1; i<=digits.size(); i++) {
-                ans[i] = digits[i-1];
-            }
-            return ans;
-        }
+        digits.insert(digits.begin(), 1);
         return digits;
     }
 };
