8333decbb9c53fff3453d72433513c14d3d2f7d2
Sandip Pramanik <163902046+Algorithm-Artisan-Sandip@users.noreply.github.com>
2025-02-18 19:34:11 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (27.62%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -1,16 +1,35 @@
+// Brute Force : Linear time complexity.
+// class Solution {
+// public:
+//     int findKthPositive(vector<int>& arr, int k) {
+//         int cnt = 0;
+//         int j = 0;
+//         for(int i=0; i<arr.size(); i++) {
+//             j++;
+//             while(j != arr[i]) {
+//                 cnt++;
+//                 if(cnt == k) return j;
+//                 j++;
+//             }
+//         }
+//         return j + (k-cnt);
+//     }
+// };
+
+
+// Optimised Approach : Logarithmic Time complexity
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
-        int cnt = 0;
-        int j = 0;
-        for(int i=0; i<arr.size(); i++) {
-            j++;
-            while(j != arr[i]) {
-                cnt++;
-                if(cnt == k) return j;
-                j++;
-            }
+        int low = 0, high = arr.size()-1;
+        
+        while(low <= high) {
+            int mid = low + (high-low)/2;
+            int missing = arr[mid] - (mid+1);
+            if(missing < k) low = mid+1;
+            else high = mid-1;
         }
-        return j + (k-cnt);
+
+        return low+k;
     }
 };
