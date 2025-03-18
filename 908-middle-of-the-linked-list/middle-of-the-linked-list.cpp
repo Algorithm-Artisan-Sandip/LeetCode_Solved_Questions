1e85e948c96f1b81db944b627229ad45c51b6c76
Sandip Pramanik <163902046+Algorithm-Artisan-Sandip@users.noreply.github.com>
2025-03-18 20:33:11 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (25.80%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -8,25 +8,48 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+ // Approach 1 : 
+// class Solution {
+// public:
+//     int findLenOfLL(ListNode* head) {
+//         ListNode* temp = head;
+//         int count = 0;
+//         while(temp != NULL) {
+//             count++;
+//             temp = temp->next;
+//         }
+//         return count;
+//     }
+//     ListNode* middleNode(ListNode* head) {
+//         int len = findLenOfLL(head);
+//         int count = 1;
+//         ListNode* temp = head;
+//         while(count != (len/2 + 1)) {
+//             count++;
+//             temp = temp->next;
+//         }
+//         return temp;
+//     }
+// };
+
+
+// Approach 2 : 
 class Solution {
 public:
-    int findLenOfLL(ListNode* head) {
-        ListNode* temp = head;
-        int count = 0;
-        while(temp != NULL) {
-            count++;
-            temp = temp->next;
-        }
-        return count;
-    }
     ListNode* middleNode(ListNode* head) {
-        int len = findLenOfLL(head);
-        int count = 1;
-        ListNode* temp = head;
-        while(count != (len/2 + 1)) {
-            count++;
-            temp = temp->next;
+        if(head == NULL) return NULL;
+
+        ListNode* fast = head;
+        ListNode* slow = head;
+
+        while(fast != NULL && fast->next != NULL) {
+            fast = fast->next;
+            if(fast != NULL)
+                fast = fast->next;
+            slow = slow->next;
         }
-        return temp;
+
+        return slow;
     }
 };
