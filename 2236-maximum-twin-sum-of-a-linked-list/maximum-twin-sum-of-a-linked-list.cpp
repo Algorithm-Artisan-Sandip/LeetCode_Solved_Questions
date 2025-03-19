e47d606ed86aeda08b6226e8cebaf9f8173e94c6
Sandip Pramanik <163902046+Algorithm-Artisan-Sandip@users.noreply.github.com>
2025-03-19 19:29:55 +0530

    Time: 3 ms (68.22%) | Memory: 138.5 MB (14.23%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.cpp
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.cpp
@@ -1,29 +1,64 @@
-class Solution {
-private:
-    ListNode* findMid(ListNode* head, stack<int>& halfOfLL) {
-        ListNode* fast = head;
-        ListNode* slow = head;
-        
-        while(fast && fast->next) {
-            halfOfLL.push(slow->val);
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-        return slow;
-    }
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 
+
+// Approach 2 : Simply using array : 
+class Solution {
 public:
     int pairSum(ListNode* head) {
-        stack<int> halfOfLL;
-        ListNode* mid = findMid(head, halfOfLL); 
-
+        vector<int> arr;
+        while(head) {
+            arr.push_back(head->val);
+            head = head->next;
+        }
         int largest = INT_MIN;
-        while(mid) {
-            int temp = mid->val + halfOfLL.top();
-            largest = max(largest, temp);
-            halfOfLL.pop();
-            mid = mid->next;
+        int size = arr.size();
+        for(int i=0; i<size/2; i++) {
+            int temp = arr[i] + arr[size-i-1];
+            largest = (largest < temp) ? temp : largest;
         }
         return largest;
     }
 };
+
+
+
+ // Approach 1 : Using Hare and Tortoise algorithm
+// class Solution {
+// private:
+//     ListNode* findMid(ListNode* head, stack<int>& halfOfLL) {
+//         ListNode* fast = head;
+//         ListNode* slow = head;
+        
+//         while(fast && fast->next) {
+//             halfOfLL.push(slow->val);
+//             slow = slow->next;
+//             fast = fast->next->next;
+//         }
+//         return slow;
+//     }
+
+// public:
+//     int pairSum(ListNode* head) {
+//         stack<int> halfOfLL;
+//         ListNode* mid = findMid(head, halfOfLL); 
+
+//         int largest = INT_MIN;
+//         while(mid) {
+//             int temp = mid->val + halfOfLL.top();
+//             largest = max(largest, temp);
+//             halfOfLL.pop();
+//             mid = mid->next;
+//         }
+//         return largest;
+//     }
+// };
+
