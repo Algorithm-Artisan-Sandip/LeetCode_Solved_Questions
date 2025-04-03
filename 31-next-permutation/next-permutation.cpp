176b26b9d6e0d264609c20a3419d59f3b773f28e
Sandip Pramanik <163902046+Algorithm-Artisan-Sandip@users.noreply.github.com>
2025-04-03 19:31:59 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (67.88%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int size = nums.size();
-        int i = size-2;
-        while(i >=0 && nums[i+1] <= nums[i]) {
-            i--;
-        }
-        if(i >= 0) {
-            int j = size-1;
-            while(nums[j] <= nums[i]) {
-                j--;
+        bool flag = false;
+        int breakPnt = -1;
+        // find the break point : 
+        for(int i=nums.size()-1; i>0; i--) {
+            if(nums[i] > nums[i-1]) {
+                breakPnt = i-1;
+                break;
             }
-            swap(nums[i], nums[j]);
         }
-        reverse(nums.begin()+i+1, nums.end());
+        // if no break point exist then simply reverse the array and return : 
+        if(breakPnt == -1) {
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+
+        // if break point exist then swap the break point element with the bigger than this which is smallest among the elements on the right of the break point : 
+        for(int i = nums.size()-1; i>breakPnt; i--) {
+            if(nums[i] > nums[breakPnt]) {
+                swap(nums[i], nums[breakPnt]);
+                break;
+            }
+        }
+        // sort all the elements to the right of the breakpoint and return : 
+        reverse(nums.begin()+breakPnt+1, nums.end());
     }
 };
