3e38e24627a22a183f38df9ac51369cab94e7b46
Sandip Pramanik <163902046+Algorithm-Artisan-Sandip@users.noreply.github.com>
2025-04-05 19:33:21 +0530

    Time: 6 ms (54.16%) | Memory: 7.7 MB (87.57%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,8 +1,11 @@
 class Solution {
+private:
+    int fibnac(int n) {
+        if(n<=1) return n;
+        return fibnac(n-1) + fibnac(n-2);
+    }
 public:
     int fib(int n) {
-        if(n == 0) return 0;
-        if(n == 1) return 1;
-        return fib(n-1) + fib(n-2);
+        return fibnac(n);
     }
 };
