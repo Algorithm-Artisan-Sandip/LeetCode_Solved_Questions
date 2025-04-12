46af08b5b2e2f67e6921453c69640ca27c2b1c65
Sandip Pramanik <163902046+Algorithm-Artisan-Sandip@users.noreply.github.com>
2025-04-12 20:43:18 +0530

    Time: 11 ms (65.50%) | Memory: 10.5 MB (84.56%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
@@ -7,22 +7,20 @@ private:
         }
      return max_or;
     }
-    void findAns(int idx, int& maxBitOr, int currBitOr, int& count, int n, vector<int> ans, vector<int>& arr) {
+    void findAns(int idx, int& maxBitOr, int currBitOr, int& count, int n, vector<int>& arr) {
         if(idx == n) {
             if(maxBitOr == currBitOr) count++;
             return;
         }
-        ans.push_back(arr[idx]);
-        findAns(idx+1, maxBitOr, (currBitOr | arr[idx]), count, n, ans, arr);
-        ans.pop_back();
-        findAns(idx+1, maxBitOr, currBitOr, count, n, ans, arr);
+        findAns(idx+1, maxBitOr, (currBitOr | arr[idx]), count, n, arr);
+        findAns(idx+1, maxBitOr, currBitOr, count, n, arr);
     }
 public:
     int countMaxOrSubsets(vector<int>& nums) {
         int maxBitOr = findMaxBitOr(nums);
         vector<int> ans;
         int count = 0;
-        findAns(0, maxBitOr, 0, count, nums.size(), ans, nums);
+        findAns(0, maxBitOr, 0, count, nums.size(), nums);
         return count;
     }
 };
