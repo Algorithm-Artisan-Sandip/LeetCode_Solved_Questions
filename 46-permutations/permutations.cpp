febe05e42f17cdc9e8bba03aa41d170beb83bed8
Sandip Pramanik <163902046+Algorithm-Artisan-Sandip@users.noreply.github.com>
2025-04-13 11:57:18 +0530

    Time: 11 ms (5.13%) | Memory: 14.4 MB (5.08%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,21 +1,27 @@
 class Solution {
 private:
-    void allPermute(vector<int>& nums, vector<vector<int>>& ans, int idx) {
-        if(idx>=nums.size()) {
-            ans.push_back(nums);
+    void findAns(vector<int>& ans, vector<vector<int>>& finalAns, vector<int>& nums, unordered_map<int, int> freq) {
+        if(ans.size() == nums.size()) {
+            finalAns.push_back(ans);
             return;
         }
-        for(int i=idx; i<nums.size(); i++) {
-            swap(nums[i],nums[idx]);
-            allPermute(nums,ans,idx+1);
-            swap(nums[i],nums[idx]);
+        for(int i=0; i<nums.size(); i++) {
+            if(!freq[nums[i]]) {
+                ans.push_back(nums[i]);
+                freq[nums[i]] = 1;
+                findAns(ans, finalAns, nums, freq);
+                freq[nums[i]] = 0;
+                ans.pop_back(); 
+            }
         }
     }
+
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> ans;
-        allPermute(nums, ans, 0);
-        sort(ans.begin(),ans.end());
-        return ans;
+        vector<int> ans;
+        vector<vector<int>> finalAns;
+        unordered_map<int, int> freq;
+        findAns(ans, finalAns, nums, freq);
+        return finalAns;
     }
 };
