0b8c304eff247a14175a610758215c16c0bded0e
Sandip Pramanik <163902046+Algorithm-Artisan-Sandip@users.noreply.github.com>
2025-04-13 12:55:41 +0530

    Time: 4 ms (54.60%) | Memory: 14.9 MB (27.47%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,11 +1,14 @@
 class Solution {
 private:
-    void findAns(int idx, set<vector<int>>& ans, vector<int>& nums) {
+    void findAns(int idx, vector<vector<int>>& ans, vector<int>& nums) {
         if(idx == nums.size()) {
-            ans.insert(nums);
+            ans.push_back(nums);
             return;
         }
+        unordered_set<int> used;
         for(int i=idx; i<nums.size(); i++) {
+            if(used.count(nums[i])) continue;  // to avoid duplicate entires
+            used.insert(nums[i]);
             swap(nums[idx], nums[i]);
             findAns(idx+1, ans, nums);
             swap(nums[idx], nums[i]);
@@ -14,9 +17,8 @@ private:
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        set<vector<int>> ans;
+        vector<vector<int>> ans;
         findAns(0, ans, nums);
-        vector<vector<int>> finalAns(ans.begin(), ans.end());
-        return finalAns;
+        return ans;
     }
 };
