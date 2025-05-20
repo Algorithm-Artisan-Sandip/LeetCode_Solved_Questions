ac8e097dc77e2b1b116d165007eb1af604227595
Sandip Pramanik <163902046+Algorithm-Artisan-Sandip@users.noreply.github.com>
2025-05-20 08:37:40 +0530

    Time: 1 ms (53.66%) | Memory: 15.5 MB (17.59%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,25 +1,24 @@
 class Solution {
 private:
-    void findCombSum(int idx, int target, int n, vector<int> ans, set<vector<int>>& finalAns, vector<int> arr) {
+    void findCombSum(int idx, int target, vector<int> ans, vector<vector<int>>& finalAns, vector<int>& candidates) {
         if(target == 0) {
-            finalAns.insert(ans);
+            finalAns.push_back(ans);
             return;
         }
-        for(int i=idx; i<n; i++) {
-            if(i>idx && arr[i] == arr[i-1]) continue;
-            if(target < arr[i]) break;
-            ans.push_back(arr[i]);
-            findCombSum(i+1, target-arr[i], n, ans, finalAns, arr);
+        for(int i=idx; i<candidates.size(); i++) {
+            if(i>idx && candidates[i] == candidates[i-1]) continue;
+            if(candidates[i] > target) break;
+            ans.push_back(candidates[i]);
+            findCombSum(i+1, target-candidates[i], ans, finalAns, candidates);
             ans.pop_back();
         }
     }
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         sort(candidates.begin(), candidates.end());
-        set<vector<int>> finalAns;
         vector<int> ans;
-        findCombSum(0, target, candidates.size(), ans, finalAns, candidates);
-        vector<vector<int>> combSum(finalAns.begin(), finalAns.end());
-        return combSum;
+        vector<vector<int>> finalAns;
+        findCombSum(0, target, ans, finalAns, candidates);
+        return finalAns;
     }
 };
