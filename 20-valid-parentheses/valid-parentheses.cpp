606310882236ae768ca49ce0d7df60b37d7bd731
Sandip Pramanik <163902046+Algorithm-Artisan-Sandip@users.noreply.github.com>
2025-06-09 07:44:52 +0530

    Time: 3 ms (7.71%) | Memory: 10.1 MB (5.36%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
-        for(char ch : s) {
-            if(ch == '(' || ch == '[' || ch == '{') st.push(ch);
-            else{
+        stack<int> st;
+        for(int i=0; i<s.size(); i++) {
+            if(s[i] == '(' || s[i] == '{' || s[i] == '[') st.push(s[i]);
+            else {
                 if(st.empty()) return false;
-                if(ch == ')' && st.top() != '(') return false;
-                else if(ch == '}' && st.top() != '{') return false;
-                else if(ch == ']' && st.top() != '[') return false;
+                if((s[i] == ')' && st.top() != '(') || 
+                  (s[i] == '}' && st.top() != '{') || 
+                  (s[i] == ']' && st.top() != '[')) {
+                  return false;
+                }
                 st.pop();
             }
         }
-        return st.empty(); 
+        return st.empty();
     }
 };
