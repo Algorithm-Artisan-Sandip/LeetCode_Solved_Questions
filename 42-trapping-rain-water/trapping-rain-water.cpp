876e5ca027d229d89d157eefae2463f363b4a566
Sandip Pramanik <163902046+Algorithm-Artisan-Sandip@users.noreply.github.com>
2025-06-19 08:15:46 +0530

    Time: 2 ms (22.32%) | Memory: 27.4 MB (19.81%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,20 +1,33 @@
+// Approach 1 : T.C: O(n), S.C: O(3*n)
 class Solution {
+private:
+    void prefMax(vector<int>& prefixMax, vector<int>& height) {
+        int size = height.size();
+        prefixMax[0] = height[0];
+        for(int i=1; i<size; i++) {
+            prefixMax[i] = max(prefixMax[i-1], height[i]);
+        }
+    }
+    void suffMax(vector<int>& suffixMax, vector<int>& height) {
+        int size = height.size();
+        suffixMax[size-1] = height[size-1];
+        for(int i=size-2; i>=0; i--) {
+            suffixMax[i] = max(suffixMax[i+1], height[i]);
+        }
+    }
 public:
-    int trap(vector<int>& a) {
-        int n=a.size();
-        int left=0,right=n-1;
-        int l=0,r=0,ans=0;
-        while(left<=right){
-            if(a[left]<=a[right]){
-                // left may be contrtibuting
-                l=max(l,a[left]);
-                ans+=l-a[left];
-                left++; 
-            }
-            else if(a[left]>a[right]){
-                r=max(r,a[right]);
-                ans+=r-a[right];
-                right--;
+    int trap(vector<int>& height) {
+        int size = height.size();
+        vector<int> prefixMax(height.size());
+        vector<int> suffixMax(height.size());
+        int ans = 0;
+        prefMax(prefixMax, height);
+        suffMax(suffixMax, height);
+        for(int i=0; i<size; i++) {
+            int leftMax = prefixMax[i];
+            int rightMax = suffixMax[i];
+            if(height[i] < leftMax && height[i] < rightMax) {
+                ans += min(leftMax, rightMax) - height[i];
             }
         }
         return ans;
