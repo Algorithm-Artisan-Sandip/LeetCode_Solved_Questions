031a808e42ded7289b68c88a99143f71eddbc73d
Sandip Pramanik <163902046+Algorithm-Artisan-Sandip@users.noreply.github.com>
2025-06-21 08:37:11 +0530

    Time: 27 ms (45.38%) | Memory: 85.9 MB (29.23%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,57 +1,36 @@
+// Approach 1 : T.C: O(5n), S.C: O(4n)
 class Solution {
+private:
+    void findPse(vector<int>& pse, vector<int>& heights, int size) {
+        stack<int> st;
+        for(int i=0; i<size; i++) {
+            while(!st.empty() && heights[st.top()] >= heights[i]) {
+                st.pop();
+            }
+            pse[i] = (st.empty()) ? -1 : st.top();
+            st.push(i);
+        }
+    }
+    void findNse(vector<int>& nse, vector<int>& heights, int size) {
+        stack<int> st;
+        for(int i=size-1; i>=0; i--) {
+            while(!st.empty() && heights[st.top()] >= heights[i]) {
+                st.pop();
+            }
+            nse[i] = (st.empty()) ? size : st.top();
+            st.push(i);
+        }
+    }
 public:
-    vector<int> nextSmaller(vector<int>& heights) {
-        int n = heights.size();
-        stack<int> st;
-        vector<int> ans(n);
-        st.push(n); // sentinel value for the last element
-
-        for (int i = n - 1; i >= 0; i--) {
-            while (st.top() != n && heights[st.top()] >= heights[i]) {
-                st.pop();
-            }
-            ans[i] = st.top();
-            st.push(i);
-        }
-
-        return ans;
-    }
-
-    vector<int> previousSmaller(vector<int>& heights) {
-        int n = heights.size();
-        stack<int> st;
-        vector<int> ans(n);
-        st.push(-1); // sentinel value for the first element
-
-        for (int i = 0; i < n; i++) {
-            while (st.top() != -1 && heights[st.top()] >= heights[i]) {
-                st.pop();
-            }
-            ans[i] = st.top();
-            st.push(i);
-        }
-
-        return ans;
-    }
-
     int largestRectangleArea(vector<int>& heights) {
-        if (heights.empty()) return 0;
-
-        vector<int> next = nextSmaller(heights);
-        vector<int> prev = previousSmaller(heights);
-        int maxArea = 0;
-
-        for (int i = 0; i < heights.size(); i++) {
-            int width;
-            if (next[i] == heights.size()) {
-                width = heights.size() - prev[i] - 1;
-            } else {
-                width = next[i] - prev[i] - 1;
-            }
-            int length = heights[i];
-            int area = width * length;
-            maxArea = max(maxArea, area);
+        int size = heights.size();
+        vector<int> pse(size), nse(size);
+        findPse(pse, heights, size);
+        findNse(nse, heights, size);
+        int maxi = INT_MIN;
+        for(int i=0; i<size; i++) {
+            maxi = max(maxi, heights[i]*(nse[i]-pse[i]-1));
         }
-        return maxArea;
+        return maxi;
     }
 };
