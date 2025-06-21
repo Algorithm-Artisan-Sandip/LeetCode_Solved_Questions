68c0f21e5397e4662ad18dfcca5650618bcda930
Sandip Pramanik <163902046+Algorithm-Artisan-Sandip@users.noreply.github.com>
2025-06-21 19:19:11 +0530

    Time: 10 ms (44.34%) | Memory: 20.1 MB (34.11%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -1,37 +1,51 @@
-#pragma GCC optimize("O3", "unroll-loops")
+// Optimal : Using largest Histogram Method
+// T.C: O(n^2), S.C: O(n^2)
 class Solution {
+private:
+    void computePrefixSum(vector<vector<char>>& matrix, vector<vector<int>>& prefixSum) {
+        for(int j=0; j<matrix[0].size(); j++) {
+            int sum = 0;
+            for(int i=0; i<matrix.size(); i++) {
+                if(matrix[i][j] == '1') sum++;
+                else sum = 0;
+                prefixSum[i][j] = sum;
+            }
+        }
+    }
+
+    int largestHistogram(vector<int> prefixSumRow) {
+        stack<int> st;
+        int ans = 0;
+        for(int i=0; i<prefixSumRow.size(); i++) {
+            while(!st.empty() && prefixSumRow[st.top()] > prefixSumRow[i]) {
+                int poppedEl = prefixSumRow[st.top()];
+                st.pop();
+                int pse = (st.empty()) ? -1 : st.top();
+                int nse = i;
+                ans = max(ans, poppedEl*(nse-pse-1));
+            }
+            st.push(i);
+        }
+        while(!st.empty()) {
+            int poppedEl = prefixSumRow[st.top()];
+            st.pop();
+            int pse = (st.empty()) ? -1 : st.top();
+            int nse = prefixSumRow.size();
+            ans = max(ans, poppedEl*(nse-pse-1));
+        }
+        return ans;
+    }
 public:
     int maximalRectangle(vector<vector<char>>& matrix) {
-        const unsigned short row=matrix.size(), col=matrix[0].size();
-        if (row==1 && col==1) return matrix[0][0]=='1';
-        vector<unsigned short> h(col+1);//height 
-        int maxArea=0;
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int maxArea = 0;
 
-        for(int i=0; i<row; i++){
-            vector<int> st={-1}; //stack will not be empty
-            for (int j=0; j<=col; j++){
-                // Count the successive '1's & store in h[j]
-                h[j]=(j==col||matrix[i][j]=='0')?0:h[j]+1;
-
-                // monotonic stack has at least element -1
-                while(st.size()>1 && (j==col||h[j]<h[st.back()])){
-                    const int m=st.back();
-                    st.pop_back();
-                    const int w=j-st.back()-1;
-                    const int area=h[m]*w;
-                    maxArea=max(maxArea, area);
-                }
-                st.push_back(j);
-            }
+        vector<vector<int>> prefixSum(n, vector<int>(m, 0));
+        computePrefixSum(matrix, prefixSum);
+        for(int i=0; i<matrix.size(); i++) {
+            maxArea = max(maxArea, largestHistogram(prefixSum[i]));
         }
         return maxArea;
     }
 };
-
-
-auto init = []() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return 'c';
-}();
