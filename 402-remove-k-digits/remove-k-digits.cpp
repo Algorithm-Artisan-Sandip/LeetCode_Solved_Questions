a71ddcbd761d6fe982616bf774bc2a3a246f28b5
Sandip Pramanik <163902046+Algorithm-Artisan-Sandip@users.noreply.github.com>
2025-06-22 08:38:47 +0530

    Time: 11 ms (7.92%) | Memory: 11.6 MB (52.74%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.cpp
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -1,29 +1,33 @@
+// Optimial : T.C: O(3*n) + O(k), S.C: O(2*n) 
 class Solution {
- public:
-  string removeKdigits(string num, int k) {
-    if (num.length() == k)
-      return "0";
-
-    string ans;
-    vector<char> stack;
-
-    for (int i = 0; i < num.length(); ++i) {
-      while (k > 0 && !stack.empty() && stack.back() > num[i]) {
-        stack.pop_back();
-        --k;
-      }
-      stack.push_back(num[i]);
+public:
+    string removeKdigits(string num, int k) {
+        stack<char> st;
+        for(int i=0; i<num.size(); i++) {
+            while(!st.empty() && k > 0 && (st.top()-'0') > (num[i]-'0')) {
+                st.pop();
+                k--;
+            }
+            st.push(num[i]);
+        }
+        // if still k is not zero : 
+        while(k--) {
+            st.pop();
+        }
+        // if now the stack is empty : 
+        if(st.empty()) return "0";
+        // forming the answer : 
+        string result = "";
+        while(!st.empty()) {
+            result += st.top();
+            st.pop();
+        }
+        // if there is any leading zeros : 
+        while(result.size() && result.back() == '0') result.pop_back();
+        // if the result is empty : 
+        if(!result.size()) return "0";
+        // reversing the result to get the correct order of the answer string : 
+        reverse(result.begin(), result.end());
+        return result;
     }
-
-    while (k-- > 0)
-      stack.pop_back();
-
-    for (const char c : stack) {
-      if (c == '0' && ans.empty())
-        continue;
-      ans += c;
-    }
-
-    return ans.empty() ? "0" : ans;
-  }
 };
