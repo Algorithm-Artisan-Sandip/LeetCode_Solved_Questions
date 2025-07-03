6f9b409230f6b47bde226f9da753d1d2baf014dc
Sandip Pramanik <163902046+Algorithm-Artisan-Sandip@users.noreply.github.com>
2025-07-03 10:09:19 +0530

    Time: 63 ms (32.42%) | Memory: 61.8 MB (36.22%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,39 +1,34 @@
 class Solution {
-public:
-    vector<vector<string>> partition(string s) {
-        vector<vector<string>> result;
-        vector<string> curr;
-        int lenS = s.length();
-
-        function<void(int)> explore = [&](int index) {
-            if (index >= lenS) {
-                result.push_back(curr);
-                return;
+private:
+    void findAns(int idx, string str, vector<string>& ans, 
+        vector<vector<string>>& finalAns) {
+        if(idx == str.size()) {
+            finalAns.push_back(ans);
+            return;
+        }
+        for(int i = idx; i < str.size(); i++) {
+            if(isPalindrome(str, idx, i)) {
+                ans.push_back(str.substr(idx, i - idx + 1));
+                // for the remaining substring starting from i+1
+                findAns(i + 1, str, ans, finalAns);
+                ans.pop_back(); // Backtrack
             }
-
-            for (int i = index; i < lenS; ++i) {
-                string subStr = s.substr(index, i - index + 1);
-                if (isPalindrome(subStr)) {
-                    curr.push_back(subStr);
-                    explore(i + 1);
-                    curr.pop_back();
-                }
-            }
-        };
-
-        explore(0);
-        return result;
+        }
     }
 
-private:
-    bool isPalindrome(const string& s) {
-        int left = 0, right = s.length() - 1;
-        while (left < right) {
-            if (s[left] != s[right])
-                return false;
-            ++left;
-            --right;
+    bool isPalindrome(string str, int start, int end) {
+        while(start < end) {
+            if(str[start] != str[end]) return false;
+            start++; end--;
         }
         return true;
+    }
+
+public:
+    vector<vector<string>> partition(string s) {
+        vector<string> ans;
+        vector<vector<string>> finalAns;
+        findAns(0, s, ans, finalAns);
+        return finalAns;
     }
 };
