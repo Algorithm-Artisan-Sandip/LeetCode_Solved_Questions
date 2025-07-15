1addd9dfb852a795948e8de87ac56aea3022cad7
Sandip Pramanik <163902046+Algorithm-Artisan-Sandip@users.noreply.github.com>
2025-07-15 11:09:59 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (95.73%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -9,43 +9,60 @@
  * };
  */
 class Solution {
-public:
-    int lenOfLL(ListNode* head) {
-        ListNode* temp = head;
-        int count = 0;
-        while(temp != NULL) {
-            count++;
-            temp = temp->next;
-        }
-        return count;
-    }
-    ListNode* reverseInKGroups(ListNode* head, int k) {
-        if(head == NULL || head->next == NULL) {
-            return head;
-        }
-        ListNode* prev = NULL;
+private:
+    ListNode* reverseLL(ListNode* head) {
+        ListNode* prev = nullptr;
         ListNode* curr = head;
-        ListNode* nextNode = NULL;
-        int count = 0;
-
-        if(k > lenOfLL(head)) {
-            return head;
-        }
-
-        while(count<k && curr != NULL) {
-            nextNode = curr->next;
+        while(curr != nullptr) {
+            ListNode* nextToCurr = curr->next;
             curr->next = prev;
             prev = curr;
-            curr = nextNode;
-            count++;
-        }
-
-        if(nextNode != NULL) {
-            head->next = reverseInKGroups(nextNode, k);
+            curr = nextToCurr;
         }
         return prev;
     }
+    ListNode* getKthNode(ListNode* head, int k) {
+        while(head != nullptr && k > 1) {
+            head = head->next;
+            k--;
+        }
+        return head;
+    }
+public:
     ListNode* reverseKGroup(ListNode* head, int k) {
-        return reverseInKGroups(head, k);
+        ListNode* temp = head;
+        ListNode* prevGrpTail = nullptr;
+        while(temp) {
+            // Find the k-th node from current position to check if a group exists
+            ListNode* kthNode = getKthNode(temp, k);
+            if(kthNode == nullptr) {
+                // If less than k nodes remain, connect the last group's tail to them and stop
+                if(prevGrpTail != nullptr) {
+                    prevGrpTail->next = temp;
+                }
+                break;
+            }
+            // Save the next group's head for later reconnection
+            ListNode* nextGrpHead = kthNode->next;
+            // Detach the current group from the rest of the list
+            kthNode->next = nullptr;
+            // Reverse the current group and get the new head
+            ListNode* reversedHead = reverseLL(temp);
+            // Connect the previous group's tail to the new head of this reversed group
+            if(prevGrpTail) {
+                prevGrpTail->next = reversedHead;
+            } else {
+                // If this is the first group, update the overall head
+                head = reversedHead;
+            }
+            // Connect the tail of the reversed group (which was the original temp)
+            // to the head of the next group
+            temp->next = nextGrpHead;
+            // Move prevGrpTail to the end of the current reversed group
+            prevGrpTail = temp;
+            // Move temp to the start of the next group
+            temp = nextGrpHead;
+        }
+        return head;
     }
 };
