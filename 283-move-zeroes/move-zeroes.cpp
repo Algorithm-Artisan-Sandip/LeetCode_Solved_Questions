6632abf157a0e8caf925765c463315d12f430a15
Sandip Pramanik <163902046+Algorithm-Artisan-Sandip@users.noreply.github.com>
2026-05-28 22:35:01 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (54.94%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,24 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int j = -1;
         int size = nums.size();
-        // finding the first zero : 
+        if(size < 2) return;
+        int nextPtr = 0;
         for(int i=0; i<size; i++) {
-            if(nums[i] == 0) {
-                j = i;
-                break;
-            }
-        }
-
-        if(j == -1) return;
-
-        // now swapping the zeros with non-zeros : 
-        for(int i=j+1; i<size; i++) {
             if(nums[i] != 0) {
-                swap(nums[i], nums[j]);
-                j++;
-            }
+                swap(nums[nextPtr++], nums[i]);
+            } 
         }
     }
 };
