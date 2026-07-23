8d0387cdf3be20508af40413b641c9a9f895a1c9
Sandip Pramanik <163902046+Algorithm-Artisan-Sandip@users.noreply.github.com>
2026-07-23 09:14:29 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (77.86%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
+    // Binary Search Optimization : 
+
     int lengthOfLIS(vector<int>& nums) {
         vector<int> list;
         list.push_back(nums[0]);
-        
+
         for(int i=1; i<nums.size(); i++) {
             auto it = lower_bound(list.begin(), list.end(), nums[i]);
 
